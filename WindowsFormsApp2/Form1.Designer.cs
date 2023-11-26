7bf57aa3c76108c70f1b9bf7ad051f2dbb8d2e46
muratapk <muratapk@hotmail.com>
2023-11-26 10:50:31 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsonuc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsonuc
+            // 
+            this.btnsonuc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnsonuc.Location = new System.Drawing.Point(331, 170);
+            this.btnsonuc.Name = "btnsonuc";
+            this.btnsonuc.Size = new System.Drawing.Size(93, 48);
+            this.btnsonuc.TabIndex = 0;
+            this.btnsonuc.Text = "Göster";
+            this.btnsonuc.UseVisualStyleBackColor = true;
+            this.btnsonuc.Click += new System.EventHandler(this.btndinle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsonuc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsonuc;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndinle_Click(object sender, EventArgs e)
+        {
+            string adi, soyadi;
+            string okul;
+            int a = 10;
+            int b = 20;
+            double c = 30;
+            decimal d=40;
+            bool durum = true;
+            int toplam = a + b;
+            float kesirli = 35;
+            string meslek = "Yazılımcı";
+            int sayi, sayi2, sayi3;
+            MessageBox.Show("Mesajımızı", "Başlık", MessageBoxButtons.RetryCancel, MessageBoxIcon.Question);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
